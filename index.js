de06bc5678ab06286b29a880a636162340281e8a
Mu-nah <102527479+Mu-nah@users.noreply.github.com>
2025-08-11 06:56:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const WEBHOOK_URL = "https://n8n-fk9q.onrender.com/webhook/94a58564-557a-478a-9eb0-a478bbd330e8"; // 🔁 Replace this with your actual n8n webhook
+const WEBHOOK_URL = "https://n8nxai.onrender.com/webhook/94a58564-557a-478a-9eb0-a478bbd330e8"; // 🔁 Replace this with your actual n8n webhook
 let lastSentId = "";
 
 const sleep = ms => new Promise(res => setTimeout(res, ms));
